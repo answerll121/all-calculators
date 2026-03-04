24ee61ff33a07a77ebd9c3f07dc2685b565ae056
answerll121 <answerll121@users.noreply.github.com>
2026-03-04 21:58:44 +0900

    chore: cleanup tasks

--- fix_duplicates.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/i18n.js', 'utf8');
-c = c.replace(/("label_diff_seconds":\s*"[^"]+",)(\s*"label_diff_seconds":\s*"[^"]+",)+/g, '$1');
-fs.writeFileSync('src/i18n.js', c);
-console.log('Fixed duplicates in i18n.js');
